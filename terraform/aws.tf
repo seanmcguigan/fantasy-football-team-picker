7d176a1269e96dfe25228cf46be8f9bf04ee76c0
Sean McGuigan <seanmcguigan@gmx.com>
2022-04-11 15:34:55 +0100

    plff

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     encrypt        = true
-    bucket         = "tf-remote-bucket-gonk"
+    bucket         = "tf-remote-bucket-gonkers"
     dynamodb_table = "tf-lock-table"
     region         = "us-east-1"
     key            = "terraform.tfstate"
